1f2727b5ee00575f80fc28753e34503cabd37879
sudheer_ranga <67502240+sudheerranga0512@users.noreply.github.com>
2020-07-16 20:55:04 +0530

    Add files via upload

---
+++ Day3/day3.js
@@ -0,0 +1,39 @@
+var restaurant=[];
+restaurant=[ 
+{
+res_name : "CHILLI RESTAURANT",
+address  : "old bipass road",
+city     : "yellamanchili"
+},
+{ 
+res_name : "BIRYANIS",
+address  : "Benz circle",
+city     : "Vijayawada"
+},
+{ 
+res_name : "SOUTHERN SPICE",
+address  : "marthalli bridge",
+city     : "BANGALORE"
+},
+{
+res_name : "BBQ NATION",
+address  : "near charminar",
+city     : "Hyderabad"
+    
+}, 
+{
+res_name : "subbaya HOTEL",
+address  : "diamond park,shankaramattam road",
+city     : "Visakhapatnam"
+    
+}
+] 
+function restaurant(restaurant){ 
+ 
+    for(var i=0;i<=restaurant.length;i++){ 
+        console.log(restaurant[i]);
+        console.log("\n");
+
+    }
+
+}
